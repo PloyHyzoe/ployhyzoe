2da29593420bed41bd21accaed0aac4a83dc97f4
PloyHyzoe <ploy301235@gmail.com>
2018-03-20 16:23:44 +0700

    20March

--- app/src/main/java/com/example/administrator/ployhyzoe/MainActivity.java
+++ app/src/main/java/com/example/administrator/ployhyzoe/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.administrator.ployhyzoe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.administrator.ployhyzoe.fregment.MainFregment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+//          Add fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add (R.id.contentmainfragment,new MainFregment())
+                    .commit();
+
+
+
+        }
+
+
+
+    } // main medthod
+
+
+
+
+
+
+} // main class
---
+++ app/src/main/java/com/example/administrator/ployhyzoe/fregment/MainFregment.java
@@ -0,0 +1,26 @@
+package com.example.administrator.ployhyzoe.fregment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.ployhyzoe.R;
+
+/**
+ * Created by Administrator on 20/3/2561.
+ */
+
+public class MainFregment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fregment_name,container,false);
+        return view;
+    }
+}//main class
