fe930d581eb7656536fba878abd879c00d073c23
PloyHyzoe <ploy301235@gmail.com>
2018-03-21 12:14:32 +0700

    RegFregment

---
+++ app/src/main/java/com/example/administrator/ployhyzoe/fregment/RegisterFregment.java
@@ -0,0 +1,24 @@
+package com.example.administrator.ployhyzoe.fregment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.ployhyzoe.R;
+
+/**
+ * Created by Administrator on 21/3/2561.
+ */
+
+public class RegisterFregment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fregment_register, container, false);
+                return view;
+    }
+}  //mainclass
