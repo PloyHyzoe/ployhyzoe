dd8e4c19a80df92b33a3807982e90e2fb72c0ee3
PloyHyzoe <ploy301235@gmail.com>
2018-03-22 09:18:12 +0700

    Create Toolbar2

--- app/src/main/java/com/example/administrator/ployhyzoe/fregment/RegisterFregment.java
+++ app/src/main/java/com/example/administrator/ployhyzoe/fregment/RegisterFregment.java
@@ -9,18 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
 
 import com.example.administrator.ployhyzoe.MainActivity;
 import com.example.administrator.ployhyzoe.R;
 import com.example.administrator.ployhyzoe.utility.MyAlert;
 
+
 /**
- * Created by Administrator on 21/3/2561.
+ * Created by masterung on 21/3/2018 AD.
  */
 
-public class RegisterFregment extends Fragment {
+public class RegisterFregment extends Fragment{
 
     //    Explicit
     private String nameString, userString, passwordString;
@@ -29,15 +28,52 @@ public class RegisterFregment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+//        Register Controller
+        registerController();
+
+
+    }   // Main Method
+
+    private void registerController() {
+        Button button = getView().findViewById(R.id.BTreg);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Get Value From EditText
+                EditText nameEditText = getView().findViewById(R.id.edtname);
+                EditText userEditText = getView().findViewById(R.id.edtuser);
+                EditText passwordEditText = getView().findViewById(R.id.edtpass);
+
+//                Change EditText to String
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+//                Check Space
+                if (nameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog("Have Space", "Please Fill All Blank");
+                } else {
+//                    No Space
+
+                }
+
+
+
+            }
+        });
     }
-        //        Create Toolbar
-
-
-
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarregister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
 
 //        Setup Title
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -51,50 +87,13 @@ public class RegisterFregment extends Fragment {
             public void onClick(View v) {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
-
-    //register controller
-    private void registerController() {
-        Button button = getView().findViewById(R.id.BTreg);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-
-
-            //                Get Value From EditText
-            EditText nameEditText = getView().findViewById(R.id.edtname);
-            EditText userEditText = getView().findViewById(R.id.edtuser);
-            EditText passwordEditText = getView().findViewById(R.id.edtpass);
-
-//                Change EditText to String
-            nameString = nameEditText.getText().toString().trim();
-            userString = userEditText.getText().toString().trim();
-            passwordString = passwordEditText.getText().toString().trim();
-
-//                Check Space
-                if (nameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) {
-//                    Have Space
-                MyAlert myAlert = new MyAlert(getActivity());
-                myAlert.myDialog("Have Space", "Please Fill All Blank");
-            } else {
-//                    No Space
-
-            }
-
-
-
-        }
-    });
-        }
-
-
-        }
+        });
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fregment_register, container, false);
-                return view;
+        return view;
     }
-}  //mainclass
+}   //Main Class
